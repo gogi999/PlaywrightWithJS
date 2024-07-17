548276bd0f8ad77702db3ee58fb30d359aa1eafa
gogi999 <g.petrovic9@gmail.com>
2024-07-17 17:14:00 +0200

    Updated Screenshots2.spec.js

--- tests/videos.spec.js
+++ tests/videos.spec.js
@@ -5,7 +5,6 @@ test('How to record videos for tests', async ({ page }) => {
     await page.goto('https://demoblaze.com/index.html#')
     
     await page.getByRole('link', { name: 'Log in' }).click()
-    await page.locator('#loginusername').fill(process.env.USERNAME)
-    await page.locator('#loginpassword').fill(process.env.PASSWORD)
+    
     await page.getByRole('button', { name: 'Log in' }).click()
 })
